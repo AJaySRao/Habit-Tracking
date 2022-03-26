3efd3ddf3201b8858f045a254c299b6f05599a72
changeAJay <ajajay008@gmail.com>
2022-03-26 09:13:44 +0530

    Advanced authentication using HTTP Header

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import requests
 
-pixel_endpoint = "https://pixe.la/v1/users"
+pixela_endpoint = "https://pixe.la/v1/users"
+TOKEN = "hmgv54asf45sdaf4532fsfdd3"
+USERNAME = "jakelong"
 
 user_param = {
-    "token": "hmgv54asf45sdaf4532fsfdd3",
-    "username": "jakelong",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-# new_user = requests.post(url=pixel_endpoint, json=user_param)
+# new_user = requests.post(url=pixela_endpoint, json=user_param)
 # print(new_user.text)
 # {"message":"Success. Let's visit https://pixe.la/@jakelong , it is your profile page!","isSuccess":true}
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_param = {
+    "Id" : "graph3ode",
+    "name": "Programming",
+    "unit": "commit",
+    "type": "int",
+    "color": "sora"
+}
+
+graph_header = {
+    "X-USER-TOKEN": TOKEN
+}
+
+graph_response = requests.post(url=graph_endpoint, json=graph_param, headers=graph_header)
+print(graph_response.text)
